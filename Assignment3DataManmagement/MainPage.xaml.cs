4d21c791b35ccd0349e6166bf15c498603d8e510
annathmk <annath.mkh@gmail.com>
2019-08-12 14:07:09 -0400

    initial commit

---
+++ Assignment3DataManmagement/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment3DataManmagement.Models;
+using GraphQL.Client;
+using GraphQL.Common.Request;
+using Xamarin.Forms;
+
+namespace Assignment3DataManmagement
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // ResponseLabel.Text = "Fetching data from github";
+
+            var qRequest = new GraphQLRequest
+            {
+                Query = @"{allFilms {films {title director} } }"
+            };
+            String url = "https://swapi.apis.guru/graphiql";
+            var graphQLClient = new GraphQLClient(url);
+            var graphQLResponse = await graphQLClient.PostAsync(qRequest);
+            Post_List.ItemsSource = graphQLResponse.Data.allFilms.films.ToObject<List<Film>>();
+        }
+    }
+}
---
+++ Assignment3DataManmagement/Models/Film.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Assignment3DataManmagement.Models
+{
+    public class Film
+    {
+
+        public string title { get; set; }
+        public string director { get; set; }
+
+        public Film()
+        {
+        }
+    }
+}
